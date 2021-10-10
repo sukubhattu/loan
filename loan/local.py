638bc18f66de9b7e3f81d351aa7b2a7523887a85
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-10 07:58:52 +0545

    Settings for debug toolbar, db, media and static files

--- loan/local.py
+++ loan/local.py
@@ -1,1 +1,25 @@
 from .base import *
+
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+
+# static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
+# auto field for db
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# debug toolbar
+INTERNAL_IPS = ['127.0.0.1', 'localhost']

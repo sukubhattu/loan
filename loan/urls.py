54264cb16b6b04366972f6c77e7f57b233815640
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-10 08:23:32 +0545

    Add users url

--- loan/urls.py
+++ loan/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('users.urls')),
     path('__debug__/', include(debug_toolbar.urls)),  # for debug urls
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

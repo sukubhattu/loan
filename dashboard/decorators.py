f40cbdc6aceb160d0c3ecface996a18348470280
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-12 00:44:37 +0545

    Redirect to fill profile details if profile is not complete

--- dashboard/decorators.py
+++ dashboard/decorators.py
@@ -1,6 +1,7 @@
 from functools import wraps
 
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 from users.models import UserProfile
 
 
@@ -8,8 +9,7 @@ def profile_complete_required(view_func):
     def wrapper_func(request, *args, **kwargs):
         user_profile = UserProfile.objects.get(user_id=request.user.id)
         if not user_profile.is_complete:
-
-            print("Profile is not complete")
+            return redirect('users:details')
         else:
             print("Profile is  complete")
             return view_func(request, *args, **kwargs)

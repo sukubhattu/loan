8b7903c724352d64da0b4c77321cb9f6e6296467
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-11 11:04:10 +0545

    Add user profile and email to user while creating user from admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+
+
+class UserAdmin(UserAdmin):
+    add_fieldsets = ((None, {'classes': ('wide',), 'fields': ('username', 'password1', 'password2', 'email')}),)
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+
+try:
+    admin.site.unregister(User)
+except:
+    pass
+
+admin.site.register(User, UserAdmin)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(UserProfile, UserProfileAdmin)

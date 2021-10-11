49b6a14696f8bd707293d0eeec8d2f3889ec59cd
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-11 20:22:43 +0545

    Include misc urls

--- loan/urls.py
+++ loan/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('misc.urls')),
     path('', include('users.urls')),
     path('', include('dashboard.urls')),
     path('accounts/', include('allauth.urls')),

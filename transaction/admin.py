c42218db17804c35830c66a231e5b86e18836736
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-12 16:39:20 +0545

    Add customization to admin of transaction

--- transaction/admin.py
+++ transaction/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class LoanOrderAdmin(admin.ModelAdmin):
+    list_display = ['loan_id', 'user', 'loan_status', 'loan_type', 'is_approved']
+    list_filter = ['loan_status', 'is_approved', 'loan_type']
+    search_fields = ['user__first_name', 'user__last_name', 'loan_id']
+
+
+class LoanOrderItemAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(LoanOrder, LoanOrderAdmin)
+admin.site.register(LoanOrderItem, LoanOrderItemAdmin)

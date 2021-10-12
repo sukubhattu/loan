486b5c24d4f0192730aed2da79396130a4ed06d1
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-12 15:31:10 +0545

    Pass user_profile in context

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from users.models import UserProfile
+
 from .decorators import profile_complete_required
 
 
 @login_required
 @profile_complete_required
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    user_profile = UserProfile.objects.get(user_id=request.user.id)
+    context = {'user_profile': user_profile}
+    return render(request, 'dashboard/dashboard.html', context)

48fe51b4eb9fff4ea5009a0598b15e6cf331b455
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-12 16:38:52 +0545

    Add LoanOrder and LoanOrderItem model

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+from .constants import LOAN_TYPE, LOAN_STATUS
+
+from users.models import UserProfile
+
+
+class LoanOrder(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_loan')
+    loan_id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    loan_amount = models.FloatField()
+    loan_term = models.PositiveIntegerField()
+    loan_type = models.CharField(max_length=2, choices=LOAN_TYPE)
+    loan_status = models.CharField(max_length=20, default='processing', choices=LOAN_STATUS)
+    is_approved = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class LoanOrderItem(models.Model):
+    loan = models.ForeignKey(LoanOrder, on_delete=models.CASCADE, related_name='loan')
+    installment_amount = models.FloatField()
+    created = models.DateTimeField(auto_now_add=True)

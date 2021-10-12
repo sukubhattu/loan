4c82b72bcd0fcc576bf31c7ae198ad31c84ab7d1
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-12 19:57:47 +0545

    Add url for stripe payment

--- misc/urls.py
+++ misc/urls.py
@@ -3,4 +3,10 @@ from .views import *
 
 app_name = 'misc'
 
-urlpatterns = [path('', index, name='index')]
+urlpatterns = [
+    path('', index, name='index'),
+    path('checkout_stripe', checkout_stripe, name='checkout_stripe'),
+    path('stripe_checkout_session', stripe_checkout_session, name='stripe_checkout_session'),
+    path('success/', success),
+    path('cancelled/', cancelled),
+]

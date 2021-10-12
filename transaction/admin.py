1151fa540399a3109c1ec2a6b63013359fbc790a
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-12 20:39:12 +0545

    Update admin fields

--- transaction/admin.py
+++ transaction/admin.py
@@ -4,8 +4,9 @@ from .models import *
 
 
 class LoanOrderAdmin(admin.ModelAdmin):
+    model = LoanOrder
     list_display = ['loan_id', 'user', 'loan_status', 'loan_type', 'is_approved']
-    list_filter = ['loan_status', 'is_approved', 'loan_type']
+    list_filter = ['loan_status', 'is_approved', 'is_complete', 'loan_type']
     search_fields = ['user__first_name', 'user__last_name', 'loan_id']
 
 

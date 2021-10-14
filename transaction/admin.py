ebf86f17e04a31978f248822f7201d5efe064566
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-15 01:18:44 +0545

    Add necessary fields and update create method

--- transaction/admin.py
+++ transaction/admin.py
@@ -5,13 +5,14 @@ from .models import *
 
 class LoanOrderAdmin(admin.ModelAdmin):
     model = LoanOrder
-    list_display = ['loan_id', 'user', 'loan_status', 'loan_type', 'is_approved']
+    list_display = ['loan_id', 'user', 'loan_status', 'loan_type', 'is_approved', 'is_complete']
     list_filter = ['loan_status', 'is_approved', 'is_complete', 'loan_type']
     search_fields = ['user__first_name', 'user__last_name', 'loan_id']
 
 
 class LoanOrderItemAdmin(admin.ModelAdmin):
-    pass
+    model = LoanOrderItem
+    list_display = ['loan']
 
 
 admin.site.register(LoanOrder, LoanOrderAdmin)
--- transaction/models.py
+++ transaction/models.py
@@ -11,6 +11,7 @@ class LoanOrder(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_loan')
     loan_id = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     loan_amount = models.FloatField()
+    loan_remaining_amount = models.FloatField()
     loan_term = models.PositiveIntegerField()
     loan_type = models.CharField(max_length=2, choices=LOAN_TYPE)
     loan_status = models.CharField(max_length=20, default='processing', choices=LOAN_STATUS)
@@ -28,10 +29,17 @@ class LoanOrder(models.Model):
             user_id=user_profile.id, loan_status='processing', is_approved=False, loan_type=loan_type
         )
         incomplete_loan_orders.update(loan_status='ignored')
-        cls.objects.create(user=user_profile, loan_amount=loan_amount, loan_term=loan_term, loan_type=loan_type)
+        cls.objects.create(
+            user=user_profile,
+            loan_amount=loan_amount,
+            loan_remaining_amount=loan_amount,
+            loan_term=loan_term,
+            loan_type=loan_type,
+        )
 
 
 class LoanOrderItem(models.Model):
-    loan = models.ForeignKey(LoanOrder, on_delete=models.CASCADE, related_name='loan')
+    loan = models.ForeignKey(LoanOrder, on_delete=models.CASCADE, related_name='loan_order')
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_loan_item')
     installment_amount = models.FloatField()
     created = models.DateTimeField(auto_now_add=True)

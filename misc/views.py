e8ac7c10d7e7a668aa91652fd3e6eac8ac2826f7
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-15 01:19:51 +0545

    Fix payment

--- misc/views.py
+++ misc/views.py
@@ -3,9 +3,14 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
 from django.http import JsonResponse
+
+import math
 import stripe
+import uuid
 
 from .models import UserLoanInstallmentPaymentSession
+from users.models import UserProfile
+from transaction.models import LoanOrder, LoanOrderItem
 
 
 def index(request):
@@ -23,7 +28,16 @@ def checkout_stripe(request):
 @csrf_exempt
 @login_required
 def stripe_checkout_session(request):
-    customer = request.user
+    loan_order = []
+    total = 0
+    user_profile = UserProfile.objects.get(user_id=request.user.id)
+    try:
+        loan_order = LoanOrder.objects.filter(user=user_profile, loan_status='processing', is_complete=False)[0]
+        total = (loan_order.loan_amount / loan_order.loan_term) * 100
+        total = math.trunc(total)
+        total = str(total)
+    except:
+        pass
     if request.method == 'GET':
         domain_url = 'http://localhost:8000/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
@@ -34,12 +48,13 @@ def stripe_checkout_session(request):
                 cancel_url=domain_url + 'cancelled/',
                 payment_method_types=['card'],
                 mode='payment',
+                client_reference_id=loan_order.loan_id,
                 line_items=[
                     {
-                        'name': 'mero bill amout',
+                        'name': loan_order.loan_id,
                         'quantity': 1,
-                        'currency': 'aud',
-                        'amount': 2000,
+                        'currency': 'usd',
+                        'amount': total,
                     }
                 ],
             )
@@ -50,21 +65,30 @@ def stripe_checkout_session(request):
 
 @login_required
 def success(request):
-    # stripe.api_key = settings.STRIPE_SECRET_KEY
-    # customer = request.user
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    user_loan_installment_payment_session_ids = list(
+        UserLoanInstallmentPaymentSession.objects.values_list('session_id', flat=True)
+    )
+    try:
+        session = stripe.checkout.Session.retrieve(request.GET['session_id'])
+        uuid_ = uuid.UUID(session['client_reference_id'])
+        session_id = request.GET['session_id']
+        paid_amount = session['amount_total'] / 100
 
-    # user_loan_installment_payment_session_ids = list(
-    #     UserLoanInstallmentPaymentSession.objects.values_list('session_id', flat=True)
-    # )
-    # try:
-    #     session = stripe.checkout.Session.retrieve(request.GET['session_id'])
-    #     session_id = request.GET['session_id']
-    #     if session_id not in user_loan_installment_payment_session_ids and session['payment_status'] == 'paid':
-    #         # logic to decrease amount
-    #         UserLoanInstallmentPaymentSession.objects.create(session_id=session_id)
+        if session_id not in user_loan_installment_payment_session_ids and session['payment_status'] == 'paid':
+            loan_order = LoanOrder.objects.get(loan_id=uuid_, is_complete=False)
+            loan_order.loan_remaining_amount -= paid_amount
+            loan_order.save()
+            user_profile = UserProfile.objects.get(user_id=request.user.id)
+            LoanOrderItem.objects.create(loan=loan_order, user=user_profile, installment_amount=paid_amount)
 
-    # except Exception as e:
-    #     return redirect('misc:index')
+            if loan_order.loan_remaining_amount <= 0:
+                loan_order.is_complete = True
+                loan_order.save()
+            UserLoanInstallmentPaymentSession.objects.create(session_id=session_id)
+
+    except Exception as e:
+        return redirect('misc:index')
     context = {'message': 'Thank you for paying your installment'}
     return render(request, 'misc/success.html', context)
 

0cd5a9f038e7cf7e93119aac81393f1bd544ebd1
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-15 01:18:09 +0545

    Add custom template tag to get loan status of current user is

--- transaction/templatetags/custom_tags_transaction.py
+++ transaction/templatetags/custom_tags_transaction.py
@@ -2,7 +2,12 @@ from django import template
 
 register = template.Library()
 
+from users.models import UserProfile
+from transaction.models import LoanOrder
+
 
 @register.filter(name='get_loan_status')
-def get_loan_status(status):
-    return 'hello'
+def get_loan_status(user_id):
+    user_profile = UserProfile.objects.get(user_id=user_id)
+    loan_orders = LoanOrder.objects.filter(user=user_profile, loan_status='processing', is_complete=False)
+    return loan_orders

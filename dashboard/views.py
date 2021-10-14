a4381f3cc0e8bb911d0296b79f09c1ebcf4370cd
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-15 01:20:33 +0545

    Pass loan data in context object

--- dashboard/views.py
+++ dashboard/views.py
@@ -6,7 +6,7 @@ from users.models import UserProfile
 from .decorators import profile_complete_required
 
 from transaction.forms import LoanOrderForm
-from transaction.models import LoanOrder
+from transaction.models import LoanOrder, LoanOrderItem
 
 
 @login_required
@@ -22,6 +22,19 @@ def dashboard(request):
             LoanOrder.create_loan_order(user_id, loan_type, loan_amount, loan_term)
     else:
         form = LoanOrderForm(initial={'loan_type': 'EL'})
+    loan_order = []
+    loan_order_items = []
     user_profile = UserProfile.objects.get(user_id=request.user.id)
-    context = {'user_profile': user_profile, 'form': form}
+    try:
+        loan_order = LoanOrder.objects.filter(user=user_profile, loan_status='processing', is_complete=False)[0]
+        loan_order_items = LoanOrderItem.objects.filter(user=user_profile)
+    except:
+        pass
+    print(loan_order)
+    context = {
+        'user_profile': user_profile,
+        'form': form,
+        'loan_order': loan_order,
+        'loan_order_items': loan_order_items,
+    }
     return render(request, 'dashboard/dashboard.html', context)

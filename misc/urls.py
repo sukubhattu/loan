eca1252e85f84e3e099861182eb11968e486407c
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-10-16 21:31:06 +0545

    Add calculator

--- misc/urls.py
+++ misc/urls.py
@@ -6,6 +6,7 @@ app_name = 'misc'
 urlpatterns = [
     path('', index, name='index'),
     path('about/', about, name='about'),
+    path('calculator/', calculator, name='calculator'),
     path('checkout_stripe', checkout_stripe, name='checkout_stripe'),
     path('stripe_checkout_session', stripe_checkout_session, name='stripe_checkout_session'),
     path('success/', success),
